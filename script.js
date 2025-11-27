f24af01b9af954c6051469b06d73c81b371ebc4f
Tarun <thakurtarun1012@gmail.com>
2025-11-26 21:28:27 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+
+    const savedUser = localStorage.getItem("username");
+    const savedPass = localStorage.getItem("password");
+
+    if (savedUser && savedPass) {
+        existingBtn.style.display = "inline-block";
+        checkbox.checked = true;
+    }
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Logged in as " + username.value);
+
+    if (checkbox.checked) {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+    } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+    }
+});
+
+existingBtn.addEventListener("click", function () {
+    const saved = localStorage.getItem("username");
+    if (saved) {
+        alert("Logged in as " + saved);
+    }
+});
